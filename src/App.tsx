6ce84ae02b4da8c5b37d3311170784285ac449b6
Brandon Chung <chung.brandon123@gmail.com>
2025-04-09 17:48:17 -0500

    Update base url path

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import CallForPapers from './components/CallForPapers';
-import ImportantDates from './components/ImportantDates';
-import Organizers from './components/Organizers';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import CallForPapers from "./components/CallForPapers";
+import ImportantDates from "./components/ImportantDates";
+import Organizers from "./components/Organizers";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/call-for-papers" element={<CallForPapers />} />
-          <Route path="/important-dates" element={<ImportantDates />} />
-          <Route path="/organizers" element={<Organizers />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router basename="/LLM-MI-MICCAI-2025">
+			<div className="min-h-screen bg-gray-50">
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/call-for-papers" element={<CallForPapers />} />
+					<Route path="/important-dates" element={<ImportantDates />} />
+					<Route path="/organizers" element={<Organizers />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;

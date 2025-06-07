16afeacc08acc17a71fce7a1c42a4eb08b6e22ba
Muhammad-Ali <mali43674@gmail.com>
2025-06-07 17:13:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,8 @@ export default defineConfig({
     {
       name: "inject-cmt-comment-and-text",
       transformIndexHtml(html) {
-        const cmtComment = `<!-- CMT-Acknowledgment: This site is intended for a CMT-supported conference -->`;
-        const cmtParagraph = `<p style="display:none;">The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>`;
-        return `${cmtComment}\n${html.replace('<div id="root"></div>', `${cmtParagraph}\n<div id="root"></div>`)}`;
-      }
+        
+
     }
   ],
   base: "/LLM-MI-MICCAI-2025",

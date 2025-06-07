542e2536a482010049c32fc5e0186eeea211fd49
Muhammad-Ali <mali43674@gmail.com>
2025-06-07 17:15:22 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,17 @@ function App() {
 		<Router basename="/">
 			<div className="min-h-screen bg-gray-50 flex flex-col">
 				<Navbar />
-				<div className="flex-grow">
+				
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/call-for-papers" element={<CallForPapers />} />
 						<Route path="/important-dates" element={<ImportantDates />} />
 						<Route path="/organizers" element={<Organizers />} />
 					</Routes>
-				</div>
+			
 
-				{/* Microsoft CMT Acknowledgment */}
-				<p className="text-xs text-center text-gray-500 px-4 py-2">
-					The Microsoft CMT service was used for managing the peer-reviewing
-					process for this conference. This service was provided for free by
-					Microsoft and they bore all expenses, including costs for Azure cloud
-					services as well as for software development and support.
-				</p>
-
+				
+				
 				<Footer />
 			</div>
 		</Router>
